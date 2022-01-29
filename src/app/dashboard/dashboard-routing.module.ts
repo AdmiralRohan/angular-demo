978ee2f55cff289c930fe7d31faa26088b044b6f
Rohan Gayen <rohangayen@gmail.com>
2022-01-29 22:43:53 +0530

    feat: add layout component in routes

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { LayoutComponent } from "../layout/containers/layout/layout.component";
 import { DashboardComponent } from "./containers/dashboard/dashboard.component";
 
-const routes: Routes = [{ path: "", component: DashboardComponent }];
+const routes: Routes = [
+	{ path: "", component: LayoutComponent, children: [{ path: "", component: DashboardComponent }] },
+];
 
 @NgModule({
 	imports: [RouterModule.forChild(routes)],

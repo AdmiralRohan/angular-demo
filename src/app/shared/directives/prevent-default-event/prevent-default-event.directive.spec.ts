d1d829f657516c40efd7ae286bb7102a32df5667
Rohan Gayen <rohangayen@gmail.com>
2022-01-30 04:09:30 +0530

    feat: add static components
    Search form static
    Pagination static
    Prevent default event directive

---
+++ src/app/shared/directives/prevent-default-event/prevent-default-event.directive.spec.ts
@@ -0,0 +1,8 @@
+import { PreventDefaultEventDirective } from "./prevent-default-event.directive";
+
+describe("PreventDefaultEventDirective", () => {
+	it("should create an instance", () => {
+		const directive = new PreventDefaultEventDirective();
+		expect(directive).toBeTruthy();
+	});
+});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,16 @@
-import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { PaginationComponent } from "./components/pagination/pagination.component";
+import { SearchFormComponent } from "./components/search-form/search-form.component";
+import { PreventDefaultEventDirective } from "./directives/prevent-default-event/prevent-default-event.directive";
+
+const components: any[] = [PaginationComponent, SearchFormComponent];
+const directives: any[] = [PreventDefaultEventDirective];
+const pipes: any[] = [];
 
 @NgModule({
-	declarations: [],
+	declarations: [...components, ...directives, ...pipes],
 	imports: [CommonModule],
+	exports: [...components, ...directives, ...pipes],
 })
 export class SharedModule {}

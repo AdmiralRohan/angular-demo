9dcddd94361f3b135e2cee215bd20dcd7eaa453a
Rohan Gayen <rohangayen@gmail.com>
2022-01-30 05:18:33 +0530

    chore: add comment

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,9 @@ import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 
+/**
+ * Singleton services
+ */
 @NgModule({
 	declarations: [],
 	imports: [CommonModule, HttpClientModule],
